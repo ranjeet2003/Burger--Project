7f19654415b58e88142fbf35d06a29ad63d427e2
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-17 20:49:36 +0530

    Adding ingredient on button click by connecting state

--- src/components/Burger/BuidControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuidControls/BuildControl/BuildControl.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import classes from './buildControl.css';
+import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={ classes.BuildControl } >
         <div className={ classes.Label } >{ props.label }</div>
         <button className={ classes.Less } >Less</button>
-        <button className={ classes.More } >More</button>
+        <button className={ classes.More } onClick = { props.added } >More</button>
     </div>
 );
 
