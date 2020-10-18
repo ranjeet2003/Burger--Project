1a1d82bf9c14e5336a9706202523ac7f0aa29b94
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-19 00:12:48 +0530

    implementing Logo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,7 +19,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
             <Button btnType="Danger" clicked = {props.purchaseCancelled} >Cancel</Button>
             <Button btnType="Success" clicked = {props.purchaseContinued}>Continue</Button>
