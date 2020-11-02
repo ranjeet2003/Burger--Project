5cd1f4c8c5142532abd918ae319bd7512f0dd8b8
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-11-02 23:45:29 +0530

    Adding two way binding to the form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,6 +10,7 @@ const input = (props) => {
                 className={classes.InputElement} 
                 {...props.elementConfig}
                 value = {props.value}
+                onChange={props.changed}
                 />;
             break;
         case('textarea'):
@@ -17,13 +18,15 @@ const input = (props) => {
                 className={classes.InputElement} 
                 {...props.elementConfig} 
                 value = {props.value}
+                onChange={props.changed}
                 />;
             break;
         case('select'):
             inputElement = (
                 <select 
                     className={classes.InputElement} 
-                    value = {props.value}>
+                    value = {props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -35,7 +38,8 @@ const input = (props) => {
             inputElement = <input 
             className={classes.InputElement} 
             {...props.elementConfig} 
-            value = {props.value} />;     
+            value = {props.value}
+            onChange={props.changed} />;     
     }
 
     return (
