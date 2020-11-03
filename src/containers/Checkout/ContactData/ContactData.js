832030ac7d75f95ce3701eed4cd7fe90e490f9ec
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-11-03 14:36:55 +0530

    Handling overall form validity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,7 +86,8 @@ class ContactData extends Component {
                         { value: 'Normal', displayValue: 'Normal' }
                     ]
                 },
-                value: ''
+                value: '',
+                valid: true
             }
         },
         loading: false
@@ -143,8 +144,14 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validatoin);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+        // console.log(updatedFormElement);
+        let formIsValid = true;
+        for(let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+        }
+        // console.log(formIsValid);
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render() {
@@ -171,7 +178,7 @@ class ContactData extends Component {
                         changed = {(event)=>this.inputChangedHandler(event, formElement.id)} 
                     />
                 ))}
-                <Button btnType="Success">ORDER</Button>
+                <Button btnType="Success" disabled = {!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if (this.state.loading) {
